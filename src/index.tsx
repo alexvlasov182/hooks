6a5b00cf828001399ff6a4ec9d4f425adc6ea512
Oleksandr Vlasov <drumlife182@gmail.com>
2024-07-19 19:30:27 +0200

    Initial project

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <div>
+      <HookSwitcher />
+    </div>
+  );
+};
+
+const HookSwitcher = () => {
+  const [color, setColor] = useState("black");
+
+  return (
+    <div style={{ padding: "10px", backgroundColor: color }}>
+      <button
+        onClick={() => {
+          setColor("black");
+        }}
+      >
+        Dark
+      </button>
+      <button
+        onClick={() => {
+          setColor("white");
+        }}
+      >
+        Light
+      </button>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
+console.log("Hello TypeScript &&");
